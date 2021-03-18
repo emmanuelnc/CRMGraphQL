11cc24b97fb32ee4c1fc72ce06a29df616da20f3
Emmanuel <emmanuelnc@live.com>
2021-03-18 08:53:23 -0600

    se configura primer mutation con graphQL,

--- dal/resolvers.js
+++ dal/resolvers.js
@@ -1,41 +1,22 @@
-//resolvers
+const Usuario = require('../models/Usuarios')
 
 const { gql } = require ('apollo-server');
 
-const cursos = [
-    {
-        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-        tecnologia: 'JavaScript ES6',
-    },
-    {
-        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        tecnologia: 'React',
-    },
-    {
-        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-        tecnologia: 'Node.js'
-    }, 
-    {
-        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-        tecnologia: 'React'
-    }
-];
-
-//resolver -- Minimo
-//resolver -- Minimo
-//parametros obtener cursos, 1 consulta anidada, 2 parametro input, 3 context compartido entre resolvers, 4 info
 const resolvers = {
-    Query: {
-           obtenerCursos: (_,{input},ctx) => {
-            console.log('input',input)   
-            console.log('Context',ctx)   
-            const resultado = cursos.filter( curso => curso.tecnologia === input.tecnologia)
-            return resultado
-           }
-           
+    
+        Query: {
+            obtenerCurso: ()=> "somethins"
+           },
+
+        Mutation: {
+                nuevoUsuario: (_, { input } ) => {
+                console.log(input)
+                return "creado..."
+                }
+}
     
         }
-}
+
 
 
 module.exports = resolvers
